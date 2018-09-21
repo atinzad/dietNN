a983096110e57fd3ac410e8296093b249e7a750f
atinzad <ahmad_baq@hotmail.com>
2018-09-21 00:20:08 -0700

    fixed loop bug in dietNN.py

--- src/model/dietNN.py
+++ src/model/dietNN.py
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     number_of_params_to_prune = precent_of_prunning/100* loaded_model.count_params()
     
     
-    for i to range(number_of_params_to_prune):
+    for i in range(number_of_params_to_prune):
         pass
