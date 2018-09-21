a6ef5ec3d0f02e2c8fdb8d48baac038a74765c9c
atinzad <ahmad_baq@hotmail.com>
2018-09-21 10:01:37 -0700

    updated dietNN.py for prunning

--- src/model/dietNN.py
+++ src/model/dietNN.py
@@ -2,29 +2,39 @@
 from argparse import ArgumentParser
 from keras.models import load_model
 from keras.models import model_from_json
+from kerassurgeon.operations import delete_channels
+from keras.utils.vis_utils import plot_model
+import copy
+import random
 
 
 
 def build_parser():
     par = ArgumentParser()
     par.add_argument('--m', type=str,
-                     dest='model_load_path', help='filepath to load model in json format', required=True)
+                     dest='model_load_path', help='filepath to load model in json format', required=True, 
+                     default="/home/atinzad/dietNN/data/raw/model.json")
     par.add_argument('--w', type=str,
-                     dest='weights_load_path', help='filepath to load weights in h5 format', required=True)
+                     dest='weights_load_path', help='filepath to load weights in h5 format', required=True,
+                     default="/home/atinzad/dietNN/data/raw/model.h5")
     par.add_argument('--c', type=int,
-                    dest='precent_of_prunning', help='percent of parameters to be prunned', required=True)
+                    dest='precent_of_prunning', help='percent of parameters to be prunned', required=True, default = 30)
 
     return par
 
 
-
+#python dietNN.py --m /home/atinzad/dietNN/data/raw/model.json --w /home/atinzad/dietNN/data/raw/model.h5 --c 30
 
 if __name__ == "__main__":
-    parser = build_parser()
-    options = parser.parse_args()
-    model_load_path = options.model_load_path
-    weights_load_path = options.weights_load_path
-    precent_of_prunning = options.precent_of_prunning
+#    parser = build_parser()
+#    options = parser.parse_args()
+#    model_load_path = options.model_load_path
+#    weights_load_path = options.weights_load_path
+#    precent_of_prunning = options.precent_of_prunning
+    
+    model_load_path = "/home/atinzad/dietNN/data/raw/model.json"
+    weights_load_path = "/home/atinzad/dietNN/data/raw/model.h5"
+    precent_of_prunning = 1
     
     print (model_load_path)
     print (weights_load_path)
@@ -39,8 +49,58 @@ if __name__ == "__main__":
     print (loaded_model.summary())
     
     
-    number_of_params_to_prune = precent_of_prunning/100* loaded_model.count_params()
+    number_of_params_to_prune = int(precent_of_prunning/100* loaded_model.count_params())
     
+    final_size =  loaded_model.count_params() - number_of_params_to_prune
     
-    for i in range(number_of_params_to_prune):
+    saved_model = copy.copy(loaded_model)
+    
+    trainable_layers = [layer for layer in saved_model.layers if len(layer.trainable_weights)>0]
+    
+    trainable_layers_max=[0]*len(trainable_layers)
+    
+    for i, layer in enumerate(trainable_layers):
+        if 'units' in layer.get_config():
+            trainable_layers_max[i]=(layer,layer.get_config()['units'])
+        elif 'filters' in layer.get_config():
+            trainable_layers_max[i]=(layer,layer.get_config()['filters'])
+            
+        
+    while saved_model.count_params() > final_size:
+        
+        current_no = saved_model.count_params()
+        #randomly choose a layer
+        layer, maxn =random.choice(trainable_layers_max)
+        
+        #randomly choose a parameter
+        i = random.randint(0,maxn-1)
+        
+        try:
+            saved_model = delete_channels(saved_model, layer, [i])
+        except:
+            pass
+        
+        print (saved_model.count_params(),layer.name, i)
+        
+        if current_no == saved_model.count_params():
+            trainable_layers = [layer for layer in saved_model.layers if len(layer.trainable_weights)>0]
+    
+            trainable_layers_max=[0]*len(trainable_layers)
+    
+            for i, layer in enumerate(trainable_layers):
+                if 'units' in layer.get_config():
+                    trainable_layers_max[i]=(layer,layer.get_config()['units'])
+                elif 'filters' in layer.get_config():
+                    trainable_layers_max[i]=(layer,layer.get_config()['filters'])
+
+
+    model_json = saved_model.to_json()
+    with open("model_small.json", "w") as json_file:
+        json_file.write(model_json)
+    saved_model.save_weights("model_small.h5")
+    print("Saved model to disk")
+    try:
+        plot_model(saved_model, to_file='vgg_small.png')
+    except:
         pass
+        
