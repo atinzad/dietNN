c6725e6564b2bdffa89fb997bf89d10d606889c6
atinzad <ahmad_baq@hotmail.com>
2018-10-01 21:36:29 -0700

    fixied git mixup issues

--- src/model/dietNN.py
+++ src/model/dietNN.py
@@ -9,7 +9,7 @@ import random
 import os
 from keras.preprocessing import image
 from keras import optimizers 
-
+import numpy as np
 
 
 def build_parser():
@@ -64,6 +64,44 @@ def prune(model, layer, rand=True,no_of_weights=0, weights=[]):
 
     model = delete_channels(model, layer, weights)
     return model
+
+
+def prune_genetic(model, layer, validation_generator, no_of_weights=1, loops=1):
+    try:
+        if 'units' in layer.get_config():
+            maxn=layer.get_config()['units']
+        elif 'filters' in layer.get_config():
+            maxn=layer.get_config()['filters']
+    except:
+        print ("layer is not a dense or conv layer")
+        return model
+    
+    results = []
+    weights_configs=[]
+    models=[]
+    data = list(range(maxn))
+    for i in range(loops):
+        random.shuffle(data)
+        weights = [data[i] for i in range(no_of_weights)]
+        try:
+            model_try = delete_channels(model, layer, weights)
+        except:
+            break
+        
+        model_try.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(),metrics=["accuracy"])
+        results.append(model_try.evaluate_generator(generator=validation_generator, steps=10)[1])
+        weights_configs.append([weights])
+        models.append(copy.copy(model_try))
+    
+   
+    try:
+        best_i = np.argmax(results)
+        model = delete_channels(model, layer, weights_configs[best_i])
+    except:
+        pass
+        
+    return model
+
                 
             
 def quantize(model, layer) :
@@ -113,12 +151,21 @@ def replace_intermediate_layer_in_keras(model, layer_id, new_layer):
 
 if __name__ == "__main__":
     
-    parser = build_parser()
-    options = parser.parse_args()
-    model_load_path = options.model_load_path
-    weights_load_path = options.weights_load_path
-    dataset_load_path = options.dataset_load_path
-    precent_of_prunning = options.precent_of_prunning
+#    parser = build_parser()
+#    options = parser.parse_args()
+#    model_load_path = options.model_load_path
+#    weights_load_path = options.weights_load_path
+#    dataset_load_path = options.dataset_load_path
+#    precent_of_prunning = options.precent_of_prunning
+#    quantization =False
+    
+    
+
+    
+    model_load_path = '~/dietNN/src/preprocess/catdog_model.json'
+    weights_load_path = '~/dietNN/src/preprocess/catdog_model.h5'
+    dataset_load_path = '~/dietNN/data/raw/catdog/test'
+    precent_of_prunning = 30
     quantization =False
     
     
@@ -156,6 +203,14 @@ if __name__ == "__main__":
     
     saved_model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(),metrics=["accuracy"])
     
+    test_datagen = image.ImageDataGenerator(rescale=1. / 255)
+    
+    validation_generator = test_datagen.flow_from_directory(
+                    dataset_load_path,
+                    target_size=(img_size,img_size),
+                    batch_size=batch_size,
+                    class_mode='categorical')
+    
     while saved_model.count_params() > final_size:
     
         trainable_layers = [layer for layer in saved_model.layers if len(layer.trainable_weights)>0]
@@ -177,19 +232,16 @@ if __name__ == "__main__":
             
             if layer.name != prediction_layer.name:
                 try:
-                    saved_model = prune(saved_model, layer, rand=True, no_of_weights=int(precent_of_prunning/100*maxn))
-                    if quantization:
-                        saved_model = quantize(saved_model, layer)
+                    saved_model = prune_genetic(saved_model, layer,  validation_generator, no_of_weights=int(precent_of_prunning/100*maxn), loops=5)
+                    #saved_model = prune(saved_model, layer, rand=True, no_of_weights=int(precent_of_prunning/100*maxn))
+                    #if quantization:
+                    #    saved_model = quantize(saved_model, layer)
                 except:
                     print ("could not process", layer.get_config()['name'])
             
-            test_datagen = image.ImageDataGenerator(rescale=1. / 255)
             
-            validation_generator = test_datagen.flow_from_directory(
-                    dataset_load_path,
-                    target_size=(img_size,img_size),
-                    batch_size=batch_size,
-                    class_mode='categorical')
+            
+            
             
 #            saved_model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(),metrics=["accuracy"])
 #            saved_model.evaluate_generator(generator=validation_generator)
@@ -199,10 +251,12 @@ if __name__ == "__main__":
     
     
     
-    
+    import time
     print (saved_model.summary())
     saved_model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(),metrics=["accuracy"])
-    print(saved_model.evaluate_generator(generator=validation_generator))
+    start = time.time()
+    print(saved_model.evaluate_generator(generator=validation_generator, steps=20))
+    print(time.time()-start)
     
     model_json = saved_model.to_json()
     with open("model_small.json", "w") as json_file:
